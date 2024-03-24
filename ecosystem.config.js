1a3c6a14224e118a2bb2a7b2ff3f8b98dad810b2
Trongnguyen-IT <nguyennhattrong@gmail.com>
2024-03-24 13:13:02 +0700

    setup cicd

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+	apps: [
+		{
+			name: 'gui-icon3dpack',
+			script: 'yarn',
+			args: 'start:prod  -p 9001',
+			interpreter: '/bin/bash',
+			instances: 1,
+			watch: false,
+			ignore_watch: ['node_modules'],
+			env: {
+				NODE_ENV: 'production',
+				NEXT_NODE_ENV: 'production',
+			},
+		},
+	],
+}
